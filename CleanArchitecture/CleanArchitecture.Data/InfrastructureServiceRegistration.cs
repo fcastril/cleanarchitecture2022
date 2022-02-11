3796796cd723ad870e1348f9a10dad166d8e2dd0
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2022-02-10 21:13:18 -0500

    Se implementa la inyección de dependencias de Infraestructure

---
+++ CleanArchitecture/CleanArchitecture.Data/InfrastructureServiceRegistration.cs
@@ -0,0 +1,35 @@
+﻿using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Models;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.Infrastructure.Repositories;
+using CleanArchitecture.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<MainContext>(
+                options => options.UseSqlServer(configuration.GetConnectionString("Default")));
+
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<ICompanyRepository, CompanyRepository>();
+            services.AddScoped<IOptionRepository, OptionRepository>();
+            services.AddScoped<IOptionSecurityRepository, OptionSecurityRepository>();
+            services.AddScoped<IProfileRepository, ProfileRepository>();
+            services.AddScoped<IUserCompanyProfileRepository, UserCompanyProfileRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+
+        }
+    }
+}
