a7f8bd0601dec938fe03eb7daefbe02e28adc0b3
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2022-02-11 07:26:07 -0500

    Definición de Identity

---
+++ CleanArchitecture/CleanArchitecture.Application/Models/Identity/AuthRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Application.Models.Identity
+{
+    public class AuthRequest
+    {
+        public string Login { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Application/Models/Identity/RegistrationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitecture.Application.Models.Identity
+{
+    public class RegistrationRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Login { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
