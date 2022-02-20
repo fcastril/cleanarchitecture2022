5bb59d47cb728f6717d96d05fb50abe6dadf5247
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2022-02-20 11:39:36 -0500

    Definir Middleware para capturar errores

---
+++ CleanArchitecture/CleanArchitecture.API/Errors/CodeErrorException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace CleanArchitecture.API.Errors
+{
+    public class CodeErrorException : CodeErrorResponse
+    {
+        public string? Details { get; set; }
+        public CodeErrorException(HttpStatusCode statusCode, string? message = null, string? details=null) 
+            : base(statusCode, message)
+        {
+            Details = details;
+        }
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using CleanArchitecture.API.Errors;
+using System.Net;
+using System.Text.Json;
+
+namespace CleanArchitecture.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                CodeErrorException response;
+
+                HttpStatusCode responseStatusCode = context.Response.StatusCode == 200
+                                                    ? HttpStatusCode.InternalServerError
+                                                    : (HttpStatusCode)context.Response.StatusCode;
+                context.Response.StatusCode = (int)responseStatusCode;
+
+                response = _env.IsDevelopment() 
+                    ? new CodeErrorException(responseStatusCode, ex.Message, ex.StackTrace)
+                    : new CodeErrorException(HttpStatusCode.InternalServerError, ex.Message);
+
+                JsonSerializerOptions options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                string json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
